af3f7bb9ef48853451f51c3ec8af97a5ec3bb0a2
Yang Zhao <zhaoyangpost@gmail.com>
2013-12-11 23:58:42 -0800

    updated: fixed bugs for more than four trains

--- bart.js
+++ bart.js
@@ -5,50 +5,65 @@ $(document).ready(function(){
   etd_num = 0;
   estimate_num = 0;
 
+
+//work: auto detect nearest station = my current location
+//work: choose from map
   $('#departure_stn').change(function() {
     my_departure = $('select#departure_stn').val();
-  });
-
-  $('#view_map').click(function() {
+    $('.train_map').html('');
     get_trains(my_departure);
-    //alert(my_departure);
   });
 });
 
 
 function get_trains(departure) { 
-  if (my_departure==='none') {
-    alert('Please select a station');
-  };
+  if (my_departure!=='none') {
 
-  $.ajax({
-    type:"GET",
-    url:"http://api.bart.gov/api/etd.aspx?cmd=etd&orig="+departure+"&key=YGKV-YKH5-YN5Q-KWMU",
-    dataType:"xml",
-    success: function(data){
-      var all_trains = [];
+    $.ajax({
+      type:"GET",
+      url:"http://api.bart.gov/api/etd.aspx?cmd=etd&orig="+departure+"&key=YGKV-YKH5-YN5Q-KWMU",
+      dataType:"xml",
+      success: function(data){
+        console.log(data);
+        var all_trains = [];
 
-      $(data).find('etd').each(function() {
-        all_trains[etd_num] = {};
-        all_trains[etd_num].dest = $(this).find('destination').text();
-        all_trains[etd_num].trains = [];
-        estimate_num=0;
-        $(this).find('estimate').each(function () {
-          all_trains[etd_num].trains[estimate_num] = {};
-          all_trains[etd_num].trains[estimate_num].color = $(this).find('hexcolor').text();
-          all_trains[etd_num].trains[estimate_num].waiting = $(this).find('minutes').text();
+        $(data).find('etd').each(function() {
+          all_trains[etd_num] = {};
 
-          estimate_num=+1;
+          all_trains[etd_num].dest = $(this).find('destination').text();
+          all_trains[etd_num].trains = [];
+
+          estimate_num=0;
+
+          $(this).find('estimate').each(function () {
+            console.log(estimate_num);
+            all_trains[etd_num].trains[estimate_num] = {};
+
+            all_trains[etd_num].trains[estimate_num].color = $(this).find('hexcolor').text();
+            all_trains[etd_num].trains[estimate_num].waiting = $(this).find('minutes').text();
+
+            estimate_num+=1;
+            console.log(estimate_num);
+
+          });
+
+          estimate_num=0;
+          etd_num+=1;
+
         });
-        estimate_num=0;
-        etd_num=+1;
-      });
-      console.log(all_trains);
-      set_trains(all_trains);
-    }
-  }); 
+
+        //console.log(all_trains);
+        etd_num=0;
+        set_trains(all_trains);
+
+      }
+    });
+  };
 };
 
+//work: beautify the map with platform
+//work: beautify the train
+//work: beautify the lanes. Combine all trains into same lanes
 function set_trains (myTrains) {
   for (var i = myTrains.length - 1; i >= 0; i--) {
     
@@ -71,13 +86,11 @@ function set_trains (myTrains) {
 
 function animate_train(trainID,waiting) {
   var animateLength = waiting*20;
-  var animateTime = waiting*1000;
-  console.log(animateLength);
+  var animateTime = waiting*1000;//should be *60
+  //console.log(animateLength);
 
   $('#' + trainID).animate({
     left: "-=" + animateLength
-  //   left: "=-"+animateLength
-  // },10000);
   },animateTime);
 }
 
