61c28e4894a27a199043b694da92002d8e4e9e8c
Yang Zhao <zhaoyangpost@gmail.com>
2013-12-12 15:42:49 -0800

    Added and reorganized lanes, platform, and shapes

--- bart.js
+++ bart.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
   my_departure = 'none';
-
   etd_num = 0;
   estimate_num = 0;
 
@@ -9,14 +8,20 @@ $(document).ready(function(){
 //work: auto detect nearest station = my current location
 //work: choose from map
   $('#departure_stn').change(function() {
+
     my_departure = $('select#departure_stn').val();
+    //my_departure = $('select#departure_stn').val();
     $('.train_map').html('');
+    $('.train_map').html('<div class="myStation"><p>Currently at ' + my_departure + '</p></div>')
     get_trains(my_departure);
+
   });
 });
 
 
+
 function get_trains(departure) { 
+
   if (my_departure!=='none') {
 
     $.ajax({
@@ -24,26 +29,28 @@ function get_trains(departure) {
       url:"http://api.bart.gov/api/etd.aspx?cmd=etd&orig="+departure+"&key=YGKV-YKH5-YN5Q-KWMU",
       dataType:"xml",
       success: function(data){
-        console.log(data);
+
+        //console.log(data);
         var all_trains = [];
 
         $(data).find('etd').each(function() {
-          all_trains[etd_num] = {};
 
+          all_trains[etd_num] = {};
           all_trains[etd_num].dest = $(this).find('destination').text();
           all_trains[etd_num].trains = [];
 
           estimate_num=0;
 
           $(this).find('estimate').each(function () {
-            console.log(estimate_num);
+
+            //console.log(estimate_num);
             all_trains[etd_num].trains[estimate_num] = {};
 
             all_trains[etd_num].trains[estimate_num].color = $(this).find('hexcolor').text();
             all_trains[etd_num].trains[estimate_num].waiting = $(this).find('minutes').text();
 
             estimate_num+=1;
-            console.log(estimate_num);
+            //console.log(estimate_num);
 
           });
 
@@ -61,22 +68,32 @@ function get_trains(departure) {
   };
 };
 
+
+
 //work: beautify the map with platform
 //work: beautify the train
 //work: beautify the lanes. Combine all trains into same lanes
 function set_trains (myTrains) {
+
   for (var i = myTrains.length - 1; i >= 0; i--) {
+
+    thisDestHTML = '<div class="each_dest"> <div class="dest_name">' + myTrains[i].dest + '</div><div class="dest_lane"><div class="dest_upcoming" id="dest' + i + '"></div></div></div>';
+    $('.train_map').append(thisDestHTML);
     
     for (var j = myTrains[i].trains.length - 1; j >= 0; j--) {
-      var thisTrainDest = myTrains[i].dest;
+
+      //var thisTrainDest = myTrains[i].dest;
       var thisTrainID = 'train' + i + j;
       var thisTrainColor = myTrains[i].trains[j].color;
       var thisTrainTime = Number(myTrains[i].trains[j].waiting);
-      var thisTrain = '<div class="each_train" id=' + thisTrainID + ">" + thisTrainDest +" Train</div>";
-      $('.train_map').append(thisTrain);
-      
-      $('#'+thisTrainID).css('background-color', thisTrainColor);
-      $('#'+thisTrainID).css('left', thisTrainTime*20);
+      var thisTrainHTML = '<div class="each_train" id=' + thisTrainID + "></div>";
+
+      $('#dest' + i).append(thisTrainHTML);
+      $('#'+thisTrainID).css({
+
+        'background-color': thisTrainColor,
+        'left': thisTrainTime*20
+      });
       
       animate_train(thisTrainID,thisTrainTime);
       
@@ -84,13 +101,18 @@ function set_trains (myTrains) {
   };  
 };
 
+
+
 function animate_train(trainID,waiting) {
+
   var animateLength = waiting*20;
-  var animateTime = waiting*1000;//should be *60
+  var animateTime = waiting*1000;//should be *60000
   //console.log(animateLength);
 
   $('#' + trainID).animate({
+
     left: "-=" + animateLength
+
   },animateTime);
 }
 
